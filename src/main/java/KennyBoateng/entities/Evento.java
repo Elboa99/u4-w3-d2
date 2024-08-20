07679d2413e47eb104bf48d4c20d3887764cbc8b
Kenny Boateng <kennyboateng.99@gmail.com>
2024-08-20 16:49:07 +0200

    classe evento

--- src/main/java/KennyBoateng/entities/Evento.java
+++ src/main/java/KennyBoateng/entities/Evento.java
@@ -1,11 +1,85 @@
 package KennyBoateng.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+enum TipoEvento {
+    PUBBLICO, PRIVATO
+}
 
 @Entity
-@Table
-
+@Table(name = "evento")
 public class Evento {
 
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String titolo;
+    private String dataEvento;
+    private String descrizione;
+
+    @Enumerated(EnumType.STRING)
+    private TipoEvento tipoEvento;
+
+    private int numeroMassimoPartecipanti;
+
+    public Evento() {
+    }
+
+    public Evento(String titolo, String dataEvento, String descrizione, TipoEvento tipoEvento, int numeroMassimoPartecipanti) {
+        this.titolo = titolo;
+        this.dataEvento = dataEvento;
+        this.descrizione = descrizione;
+        this.tipoEvento = tipoEvento;
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(String dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public TipoEvento getTipoEvento() {
+        return tipoEvento;
+    }
+
+    public void setTipoEvento(TipoEvento tipoEvento) {
+        this.tipoEvento = tipoEvento;
+    }
+
+    public int getNumeroMassimoPartecipanti() {
+        return numeroMassimoPartecipanti;
+    }
+
+    public void setNumeroMassimoPartecipanti(int numeroMassimoPartecipanti) {
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
 }
